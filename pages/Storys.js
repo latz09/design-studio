f9630d3941d4235a0e25f8e62c3ff7d4e90d8130
latz09 <85132328+latz09@users.noreply.github.com>
2023-07-25 12:41:33 -0500

    story1

---
+++ pages/Storys.js
@@ -0,0 +1,12 @@
+
+import Story1 from "../components/Storys/Story1";
+
+const Story = () => {
+	return (
+		<div className=' h-screen bg-blackTwo flex justify-between bg-black'>
+		<Story1 />
+		</div>
+	);
+};
+
+export default Story;
