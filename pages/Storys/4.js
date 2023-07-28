67842d6477b6193332b8a56286421dd65f5f927d
latz09 <85132328+latz09@users.noreply.github.com>
2023-07-28 15:11:32 -0500

    dark

--- pages/Storys/4.js
+++ pages/Storys/4.js
@@ -1,12 +1,12 @@
-import logo from '../../public/images/mockups/Final/phones.png';
+import logo from '../../public/images/mockups/grooming/web-design-in-saint-paul.png';
 import Image from 'next/image';
 const Story4 = () => {
 	return (
-		<div className='bg-cyan-300 h-screen'>
+		<div className='bg-black h-screen'>
 			<div className='flex flex-col justify-between h-full '>
                 <div className="h-full bordr"></div>
-                <div className="h-full bordr">
-                    <Image src={logo} alt='Ad Designs' className="w-4/5 mx-auto" />
+                <div className="h-full bordr opacity-40">
+                    <Image src={logo} alt='Ad Designs' className="   mx-auto" />
                 </div>
                 <div className="h-full borde"></div>
             </div>
