f8431570ead2bbcd7aded9fb79dd130028101669
latz09 <85132328+latz09@users.noreply.github.com>
2023-07-28 14:42:02 -0500

    background animation

--- pages/Storys/3.js
+++ pages/Storys/3.js
@@ -5,16 +5,16 @@ import logo from '../../public/images/logos/logo-light-color.png';
 const Story3 = () => {
     return (
         <div className="relative min-h-screen">	
-            {/* <img 
+            <img 
                 src='https://media.giphy.com/media/iZAbKh9hJ6rGE/giphy.gif' 
                 alt='Japanese Gardening' 
                 className="absolute z-0 w-full h-full object-cover"
-            /> */}
-            <div className="relative z-10 bg-black h-screen flex flex-col justify-between">
+            />
+            <div className="relative z-10 bg-black/20 h-screen flex flex-col justify-between">
                 {/* Your content goes here */}
                 <span></span>
-                <Image src={logo} alt='Ad Designs' className="opacity-80" /> 
                 <span></span>  
+                <Image src={logo} alt='Ad Designs' className="w-4/5 mx-auto" /> 
             </div>
         </div>
     );
