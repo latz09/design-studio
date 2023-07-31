cdd90557b9620c685d3598319b53a35e75f96f3c
latz09 <85132328+latz09@users.noreply.github.com>
2023-07-31 12:40:26 -0500

    d

--- pages/Ads/1.js
+++ pages/Ads/1.js
@@ -5,18 +5,18 @@ const AdOne = () => {
 	return (
 		<div className='bg- h-screen text-[#E3FCFF] font-questrial  '>
 			<div
-				className={`bg-gradient-to-t from-dark via-[#00454C] to-dark h-2/5  flex flex-col justify-between `}
+				className={`bg-gradient-to-t from-dark via-[#00454C] to-dark h-2/3  flex flex-col justify-between `}
 			>
 				<div>
-					<div className='mt-16 ml-4 scale-y-150 font-semibold tracking-wider text-5xl'>
+					<div className='mt-16 ml-4 scale-y-150 font-semibold tracking-wider text-5xl w-3/4'>
 						Need a website?
 					</div>
-					<div className='mt-16 ml-4 scale-y-150 text-xl'>
+					<div className='mt-16 ml-4 scale-y-150 text-xl w-2/3'>
 						Latz Web Design is your best option
 					</div>
 				</div>
 				<div className="flex flex-col justify-between">
-					<div className='w-1/4 mx-auto mb-6 '>
+					<div className='w-1/5 mx-auto my-6 '>
 						<Image src={logo} alt='Ad Designs' className='   mx-auto' />
 					</div>
                     <div className="bg-[#00454C] p-3 text-end">
