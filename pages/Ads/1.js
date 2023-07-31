9b63faa388e3ced5f3454f53392d6b5fb9017de4
latz09 <85132328+latz09@users.noreply.github.com>
2023-07-31 12:37:39 -0500

    2/5

--- pages/Ads/1.js
+++ pages/Ads/1.js
@@ -5,7 +5,7 @@ const AdOne = () => {
 	return (
 		<div className='bg- h-screen text-[#E3FCFF] font-questrial  '>
 			<div
-				className={`bg-gradient-to-t from-dark via-[#00454C] to-dark h-3/5  flex flex-col justify-between `}
+				className={`bg-gradient-to-t from-dark via-[#00454C] to-dark h-2/5  flex flex-col justify-between `}
 			>
 				<div>
 					<div className='mt-16 ml-4 scale-y-150 font-semibold tracking-wider text-5xl'>
@@ -16,7 +16,7 @@ const AdOne = () => {
 					</div>
 				</div>
 				<div className="flex flex-col justify-between">
-					<div className='w-1/4 mx-auto mb-12 '>
+					<div className='w-1/4 mx-auto mb-6 '>
 						<Image src={logo} alt='Ad Designs' className='   mx-auto' />
 					</div>
                     <div className="bg-[#00454C] p-3 text-end">
