521f7ea96669ee383ff31e5d2bac8f8c4b68ef1b
latz09 <85132328+latz09@users.noreply.github.com>
2023-08-29 11:34:08 -0500

    df

--- pages/before-and-afters/one/AfterOne.js
+++ pages/before-and-afters/one/AfterOne.js
@@ -1,18 +1,17 @@
-import { BsFillXDiamondFill } from 'react-icons/bs';
+import { BsFillXDiamondFill, BsHouseCheck } from 'react-icons/bs';
 import { AiOutlineMenu } from 'react-icons/ai';
+import { GiVacuumCleaner } from 'react-icons/gi';
+import { PiMaskHappy } from 'react-icons/pi';
 import heroImage from '../../../public/images/before-afters/kitchen.jpg';
 import Image from 'next/image';
-
-
 
 const AfterOne = () => {
 	return (
 		<div className='grid font-oswald bg-[#F2F7FE]'>
 			<Navbar />
 			<Hero />
-            <ShortAbout />
-            <div className="h-screen"></div>
-            
+			<ShortAbout />
+			<div className='h-screen'></div>
 		</div>
 	);
 };
@@ -34,7 +33,7 @@ const Navbar = () => {
 
 const Hero = () => {
 	return (
-		<div className='relative h-[53vh]'>
+		<div className='relative h-[50vh]'>
 			<Image
 				src={heroImage}
 				alt='Kitchen'
@@ -43,17 +42,16 @@ const Hero = () => {
 				priority={true}
 			/>
 			<div className='absolute inset-0 '></div>
-			<div className='absolute grid place-items-center h-full w-full bg-gradient-to-b from-[#011229]/10 via-[#011229]/70 to-[#011229]/10'>
-				<div className='grid  gap-2 text-[#F8F7EC] font-oswald mx-4 '>
+			<div className='absolute grid place-items-center h-full w-full bg-gradient-to-b from-[#011229]/10 via-[#011229]/50 to-[#011229]/10'>
+				<div className='grid place-items-center  gap-2 text-[#F8F7EC] font-oswald mx-4 '>
 					<div>
-                      
-						<h2 className='text-5xl '>HomeGlow</h2>
+						<h2 className='text-5xl '>Come Home Happy</h2>
 					</div>
-					<p className='font-mulish'>
+					<p className='font-mulish text-center'>
 						From dusty corners to sunny windows, we're all about that glow-up!
 					</p>
-					<button className='bg-[#F2F7FE] shadow-lg tracking-wider text-[#011229]  font-bold text-xl mt-6 px-5 py-2 rounded-sm place-self-center'>
-						Get Glowing Now
+					<button className='bg-[#2B7EED] shadow-lg tracking-wider   text-xl mt-6 px-6 py-2 rounded-sm place-self-center w-1/2'>
+						Book a Cleaning
 					</button>
 				</div>
 			</div>
@@ -61,14 +59,47 @@ const Hero = () => {
 	);
 };
 
-
 const ShortAbout = () => {
-    return (
-        <div className='mx-4 mt-8 py-4  text-cente'>
-            <h3 className='text-2xl font-semibold text-[#011229] mb-3 text-center'>Why Choose HomeGlow?</h3>
-            <p className='text-[#011229]/80 font-mulis leading-7 text-lg'>
-                At HomeGlow, we believe a clean home is a happy home. Our team is passionate about delivering top-notch cleaning services that not only meet, but exceed expectations. From those hard-to-reach spots to everyday surfaces, we guarantee a shine that feels brand new, every single time.
-            </p>
-        </div>
-    )
-}
+	return (
+		<div className='mx-4 mt-6 py-4  text-cente'>
+			<h3 className='text-2xl font-semibold text-[#011229]  text-center font-mulish mb-8'>
+				How it works?
+			</h3>
+			<div className='flex items-center justify-evenly gap-4'>
+				<div className='grid gap-4 place-items-center'>
+					<span className='text-4xl text-[#2B7EED]'>
+						<BsHouseCheck />
+					</span>
+					<div className='grid gap-1 place-items-center'>
+						<span className='opacity-90'>BOOK</span>
+						<span className='text-sm text-center opacity-80'>
+							Select the location and time you'd like your professional clean.
+						</span>
+					</div>
+				</div>
+				<div className='grid gap-4 place-items-center'>
+					<span className='text-4xl text-[#2B7EED]'>
+						<GiVacuumCleaner />
+					</span>
+					<div className='grid gap-1 place-items-center'>
+						<span className='opacity-90'>CLEAN</span>
+						<span className='text-sm text-center opacity-80'>
+							A professional cleaner comes over and cleans your home.
+						</span>
+					</div>
+				</div>
+				<div className='grid gap-4 place-items-center'>
+					<span className='text-4xl text-[#2B7EED]'>
+						<PiMaskHappy />
+					</span>
+					<div className='grid gap-1 place-items-center'>
+						<span className='opacity-90'>HAPPY</span>
+						<span className='text-sm text-center opacity-80'>
+							Come to your home and enjoy your clean home.
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
