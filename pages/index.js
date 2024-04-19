99a89c59faf2aa0e071f9efc66b6373384ae5cd7
latz09 <85132328+latz09@users.noreply.github.com>
2024-04-19 12:54:00 -0500

    d

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,149 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
-export default function Home() {
-	const pages = [
-		{
-			studio: 'Storys',
-			link: '/Storys',
-		},
-		{
-			studio: 'ads',
-			link: '/Ads',
-		},
-		{
-			studio: 'Before and Afters',
-			link: '/before-and-afters',
-		},
-	];
+const ShuffleHero = () => {
+  return (
+    <section className="w-full px-8 py-12 grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto">
+      <div>
+        <span className="block mb-4 text-xs md:text-sm text-indigo-500 font-medium">
+          Better every day
+        </span>
+        <h3 className="text-4xl md:text-6xl font-semibold">
+          Sweet JuanJo's
+        </h3>
+        <p className="text-base md:text-lg text-slate-700 my-4 md:my-6">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam nobis in
+          error repellat voluptatibus ad.
+        </p>
+        <button className="bg-indigo-500 text-white font-medium py-2 px-4 rounded transition-all hover:bg-indigo-600 active:scale-95">
+          Find a class
+        </button>
+      </div>
+      <ShuffleGrid />
+    </section>
+  );
+};
 
-	return (
-		<div className='grid  h-screen place-items-center bg-black '>
-			<div className='grid gap-4'>
-				{pages.map((page, index) => {
-					return (
-						<Link href={page.link} key={index} className='p-4 bg-white text-2xl font-semibold rounded-sm text-black/70 hover:text-white hover:bg-white/50 hover:scale-90 transition duration-700 text-center'>
-							{page.studio}
-						</Link>
-					);
-				})}
-			</div>
-		</div>
-	);
-}
+const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
+const squareData = [
+  {
+    id: 1,
+    src: "https://media.istockphoto.com/id/486912784/photo/vanilla-cupcake.jpg?s=2048x2048&w=is&k=20&c=1kFWicvkMFOuDSgRtQk2_FVACBYgkn4V1pgsMiEC45w=",
+  },
+  {
+    id: 2,
+    src: "https://media.istockphoto.com/id/1472849554/photo/birthday-cake-with-a-blue-ganache-drip-and-colorful-sprinkles-isolated-on-a-white-background.jpg?s=2048x2048&w=is&k=20&c=T9EBjkM_SaZ4LaS2lOimfgW4lpNOZAkGfJu_nWkUPjw=",
+  },
+  {
+    id: 3,
+    src: "https://media.istockphoto.com/id/517109442/photo/chocolate-chip-cookie-isolated.jpg?s=2048x2048&w=is&k=20&c=R-7te4PvTLr4Z8IEd_Hnw6VMMbv9WxYxAznUubOAZA0=",
+  },
+  {
+    id: 4,
+    src: "https://media.istockphoto.com/id/1256531872/photo/marshmallow-square-bar-on-white.jpg?s=2048x2048&w=is&k=20&c=Uc2f2N3zRdBELa5DSS2aF3BWs2J4GnQIdSPV0UKsKSg=",
+  },
+//   {
+//     id: 5,
+//     src: "https://images.unsplash.com/photo-1569074187119-c87815b476da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1325&q=80",
+//   },
+//   {
+//     id: 6,
+//     src: "https://images.unsplash.com/photo-1556817411-31ae72fa3ea0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+//   },
+//   {
+//     id: 7,
+//     src: "https://images.unsplash.com/photo-1599586120429-48281b6f0ece?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+//   },
+//   {
+//     id: 8,
+//     src: "https://plus.unsplash.com/premium_photo-1671436824833-91c0741e89c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+//   },
+//   {
+//     id: 9,
+//     src: "https://images.unsplash.com/photo-1431324155629-1a6deb1dec8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+//   },
+//   {
+//     id: 10,
+//     src: "https://images.unsplash.com/photo-1610768764270-790fbec18178?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+//   },
+//   {
+//     id: 11,
+//     src: "https://images.unsplash.com/photo-1507034589631-9433cc6bc453?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=684&q=80",
+//   },
+//   {
+//     id: 12,
+//     src: "https://images.unsplash.com/photo-1533107862482-0e6974b06ec4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=882&q=80",
+//   },
+//   {
+//     id: 13,
+//     src: "https://images.unsplash.com/photo-1560089000-7433a4ebbd64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+//   },
+//   {
+//     id: 14,
+//     src: "https://images.unsplash.com/photo-1517466787929-bc90951d0974?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
+//   },
+//   {
+//     id: 15,
+//     src: "https://images.unsplash.com/photo-1606244864456-8bee63fce472?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=681&q=80",
+//   },
+//   {
+//     id: 16,
+//     src: "https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1820&q=80",
+//   },
+];
+
+const generateSquares = () => {
+  return shuffle(squareData).map((sq) => (
+    <motion.div
+      key={sq.id}
+      layout
+      transition={{ duration: 1.5, type: "spring" }}
+      className="w-full h-full "
+      style={{
+        backgroundImage: `url(${sq.src})`,
+        backgroundSize: "cover",
+      }}
+    ></motion.div>
+  ));
+};
+
+const ShuffleGrid = () => {
+  const timeoutRef = useRef(null);
+  const [squares, setSquares] = useState(generateSquares());
+
+  useEffect(() => {
+    shuffleSquares();
+
+    return () => clearTimeout(timeoutRef.current);
+  }, []);
+
+  const shuffleSquares = () => {
+    setSquares(generateSquares());
+
+    timeoutRef.current = setTimeout(shuffleSquares, 3000);
+  };
+
+  return (
+    <div className="grid grid-cols-2 grid-rows-2 h-[450px] gap-8 ">
+      {squares.map((sq) => sq)}
+    </div>
+  );
+};
+
+export default ShuffleHero;
